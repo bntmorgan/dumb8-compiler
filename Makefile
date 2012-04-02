ecd590c3f0257eb52ae2108841ee5c0d7c58e83a
Carla Sauvanaud <sauvanau@etud.insa-toulouse.fr>
2012-04-02 14:53:01 +0200

    s de la grammaire (make test possible). Modification de la grammaire. Ajout de : expressions arithmetiques, declaration de fonctions et appels de fonctions.

--- Makefile
+++ Makefile
@@ -1,5 +1,8 @@
 all : compiler
 
+test : compiler test_grammar.c
+	cat test_grammar.c | ./compiler 
+
 yacc : syntaxic_analyzer.y
 	yacc -d -o syntaxic_analyzer.c syntaxic_analyzer.y
 
---
+++ test_grammar.c
@@ -0,0 +1,16 @@
+/****FICHIER DE TESTS*/
+
+/****Declarations*/
+a = 1;
+b = d = e = 5;
+
+/****Appels de fonctions*/
+
+f();
+f2(aa, bb, 5, 12);
+
+/***Declarations de fonctions*/
+int g();
+int g2(int i, int j);
+
+
