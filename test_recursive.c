12c0e4e4ef977e0c8a1b07e924c9ea10cd84561c
Benoît Morgan <bntmorgan@gmail.com>
2012-06-05 11:03:44 +0200

    Meilleure test_recusrsive

--- test_recursive.c
+++ test_recursive.c
@@ -2,6 +2,17 @@ int tictac(int n);
 
 int tuning(int n);
 
+int main () {
+  int a = 42;
+  tictac(10);
+  int b = 0;
+  while ( b < 10) {
+    b = tuning(b);
+    printf(b);
+  }
+  return 0;
+}
+
 int tictac(int n) {
   printf(n);
   if (n > 0) {
@@ -14,13 +25,3 @@ int tuning(int a) {
   return b;
 }
 
-int main () {
-  int a = 42;
-  tictac(10);
-  int b = 0;
-  while ( b < 10) {
-    b = tuning(b);
-    printf(b);
-  }
-  return 0;
-}
